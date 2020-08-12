264fe3b026f2b572b0668a9a2df4a518699883b3
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-08-13 00:24:00 +0530

    Added Constructors Comments

--- C++/Class-All-Types-Of-Functions/Class-Concepts.cpp
+++ C++/Class-All-Types-Of-Functions/Class-Concepts.cpp
@@ -3,12 +3,15 @@ using namespace std;
 
 class Rectangle{
     private:
+    // Declaring the Private Variables
         int length;
         int breadth;
     public:
+    // Declaring Default Constructor
         Rectangle(){
             length = breadth = 1;
         }
+    // Declaring Parameterized Constructor
         Rectangle(int l, int b);
-        
+
 };
