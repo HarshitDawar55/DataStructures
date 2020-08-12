c2adca5c40ded384c945e5351bd6159b695b8bfe
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-08-13 00:56:13 +0530

    Started defining the Template Class!

--- C++/Template-Class/Template-Class.cpp
+++ C++/Template-Class/Template-Class.cpp
@@ -5,5 +5,14 @@ using namespace std;
 // Defining the Template Class
 template <class T>
 class Mathematical_Operations{
+    private:
+        T n1, n2;
+
+    public:
+    // Defining the Constructor
+        Mathematical_Operations(T n1, T n2){
+            this->n1 = n1;
+            this->n2 = n2;
+        }
 
 };
